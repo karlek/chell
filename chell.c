40ba0739475a8caa0b4844cce2cc2f5e35cb29b6
Henry <henry@vaert.se>
2015-05-16 15:55:12 +0200

    add color; add execute; fix modularity

--- chell.c
+++ chell.c
@@ -1,151 +1,208 @@
-/* Chell is a homeage to the protagonist in the portal games. */
+/* Chell is a homage to the protagonist in the portal games. */
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 #define INP_LEN 256
 #define CMD_LEN 256
+#define ARG_LEN 256
 
-const char * prompt = "(^._.^)ﾉ > ";
+#define BLACK   "\x1b[90m"
+#define RED     "\x1b[91m"
+#define GREEN   "\x1b[92m"
+#define YELLOW  "\x1b[93m"
+#define BLUE    "\x1b[94m"
+#define MAGENTA "\x1b[95m"
+#define CYAN    "\x1b[96m"
+#define RESET   "\x1b[0m"
 
+#define NAME "chell"
+
+/* The 5 is which color index 0-7; then 0-255m.*/
+/* Hex color codes can be calculated with: COLOR = r*6^2 + g*6 + b) + 16. */
+#define SPECIAL "\x1b[38;5;150m"
+
+const char * prompt = "%s(^._.^)ﾉ%s %s@%s %s%s%s %s$%s ";
+
+int snprintf (char * s, size_t n, const char * format, ...);
 void cd(char *);
-int index(char *, char);
-char * words(char *);
-int system(char *);
+void pwd(char *, size_t);
+int isspace(int);
 void exit(int);
-char * getenv(char *);
+int parse(char *, char **);
+void checkEnv();
+void execute(char **);
+void print_prompt(char *, size_t);
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
 	/* Whole input string. */
-	char input[INP_LEN];
-/*	char tokenizer[INP_LEN];*/
+	char input[INP_LEN] = "";
 	
-	/* Command to execute. */
-	char command[CMD_LEN];
+	char *args[32]; 
+	int nwords;
 
 	/* Working directory. */
 	char wd[256];
 
-	/*char * pch;*/
-	char * pager;
-	char argument[256];
-	int ind = 0, ret = 0;
+	memset(wd, 0, 256);
 
 	while (1) {
-		/* Print prompt. */
-		printf("%s", prompt);
-	
-		/* Get user input. */
-		fgets(input, CMD_LEN, stdin);
+		print_prompt(wd, sizeof(wd));
 
-		ind = index(input, ' ');
-		if (ind != -1) {
-			strncpy(command, input, ind);
-			command[strlen(command)] = '\0';
-		} else {
-			strncpy(command, input, sizeof(command));
-			command[strlen(command)-1] = '\0';
-		}
+		/* Get user input. */
+		fgets(input, sizeof(input), stdin);
+
+		nwords = parse(input, args);
+		args[nwords] = NULL;
 
 		/* a built-in command "exit" which terminates all remaining processes
 		started from the shell in an orderly manner before exiting the shell
-		itself */
-
-		printf("command: `%s`\n", command);
-		if (strcmp("exit", command) == 0) {
+		itself */		
+		if (nwords == 0) {
+			continue;
+		}
+		if (strcmp("exit", args[0]) == 0) {
 			exit(0);
-		} else if (strcmp("cd", command) == 0) {
-			printf("%s\n", input);
-			/* Ignore the space and the command. */
-			if (ind != -1) {
-				cd(input+ind+1);			
-			} else {
-				cd(input+strlen(command)+1);			
-			}
-		} else if (strcmp("pwd", command) == 0) {
-			if (getcwd(wd, sizeof(wd)) != NULL) {
-			    fprintf(stdout, "Current working dir: %s\n", wd);
-			} else {
-			    printf("getcwd() error");
-			}
-	    } else if (strcmp("checkEnv", command) == 0) {
-	    	pager = getenv("PAGER");
-	    	if (pager == NULL) {
-	    		pager = "lessq";	
-	    	}
-	    	printf("pager: %s\n", pager);
-	    	sprintf(argument, "printenv | sort | %s", pager);
-	    	printf("arg: %s\n", argument);
-	    	ret = system(argument);
-	    	printf("ret: %d\n", ret);
-	    	if (ret != 0) {
-	    		pager = "more";
-	    	}
-	    	printf("here\n");
-	    	sprintf(argument, "printenv | sort | %s", pager);
-	    	system(argument);
-	    } else {
-	    	/* printf("input: %s\n", input); */
-	    	system(input);
-	    }
-	
-		/* Argument number. */
-		/* i = 0; */
-
-		/* Still exists an argument. */
-		/*while(pch != NULL) {*/
-			/* The first argument is always a command. */
-/*			if (i == 0) {}
-			pch = strtok (NULL, " ,.-");
-			i++;
+		} else if (strcmp("cd", args[0]) == 0) {
+			cd(args[1]);			
+		} else if (strcmp("pwd", args[0]) == 0) {
+			pwd(wd, sizeof(wd));
+		} else if (strcmp("checkEnv", args[0]) == 0) {
+			checkEnv();
+		} else {
+			execute(args);
 		}
-*/	
-		/* Null terminated strings for strcmp. 
-		ind = index(input, '\n');
-		if (ind == -1) {
-			ind = INP_LEN-1;
-		}
-		input[ind] = '\0';
-		*/
 
 		/* Zero the strings. */
-		memset(command, 0, strlen(command));
 		memset(input, 0, strlen(input));
+		/*memset(wd, 0, strlen(wd));*/
+		/*free(words);*/
 	}
 	return 0;
 }
 
-int index(char * buf, char c) {
-	int index = -1;
-	char * ptr;
-	ptr = strchr(buf, c);
-	if(ptr) {
-		index = ptr - buf;
+int parse(char *line, char **argv) {
+	/* Number of args. */
+	int argc = 0;
+
+	/* Ignore empty strings. */    
+	if (strlen(line) == 1) {
+		return argc;
 	}
-	return index;
+
+    /* Parse the whole line. */ 
+    while (*line != '\0') {
+		/* Replace whitespace with null-byte. */
+		while (*line == ' ' ||
+		       *line == '\n') {
+			argc++;
+			*line++ = '\0';
+		}
+
+		/* Save the argument. */
+		*argv++ = line;          
+
+		/* Skip until we need to remove whitespace.  */
+		while (*line != '\0' &&
+			   *line != ' '  &&
+			   *line != '\n') {
+			line++;
+		}
+    }
+	return argc;
 }
 
-char * words(char * str) {
-	char * pch;
-	pch = strtok(str, " \n");
-	return pch;
-}
-
+/* Doesn't work for ~root */
 void cd(char * input) {
 	int ret = 0;
-	printf("cd: `%s`\n", input);
-	printf("strlen: %u\n", (int)strlen(input));
-	if ((int)strlen(input) == 0) {
-		chdir(getenv("HOME"));
-		return;
-	}
-	/* Remove newline. */
-	input[strlen(input)-1] = '\0';
+	char path[256] = "";
 
-	ret = chdir(input);
-	switch (ret) {
-	case -1:	
-		printf("cd: The directory “%s” does not exist\n", input);
+	/* No argument -> go to home directory. */
+	if (input == NULL) {
+		ret = chdir(getenv("HOME"));
+	} else if ('~' == input[0]) {
+		strcat(path, "/home/");
+		strcat(path, input+1);
+		ret = chdir(path);
+	} else {
+		ret = chdir(input);
 	}
+	switch (ret) {
+		case -1:
+			fprintf(stderr, "cd: The directory “%s” does not exist\n", input);
+	}
+}
+
+void pwd(char *wd, size_t size) {
+	if (getcwd(wd, size) != NULL) {
+		printf("Current working dir: %s\n", wd);
+	} else {
+		fprintf(stderr, "getcwd: %s\n", strerror(errno));
+	}
+}
+
+/* Still broken, needs to be implemented with pipe. */
+void checkEnv() {
+	int ret = 0;
+	char argument[ARG_LEN] = "";
+	char *pager = getenv("PAGER");
+
+	if (pager == NULL) {
+		pager = "lessq";	
+	}
+	
+	printf("pager: %s\n", pager);
+
+	/* Necessary to check for errors? */
+	snprintf(argument, ARG_LEN, "printenv | sort | %s", pager);
+	printf("arg: %s\n", argument);
+	
+	ret = system(argument);
+	printf("ret: %d\n", ret);
+	if (ret != 0) {
+		pager = "more";
+	}
+	
+	snprintf(argument, ARG_LEN, "printenv | sort | %s", pager);
+	system(argument);
+}
+
+void execute(char **argv) {
+	pid_t pid;
+	int status;
+
+	/* Fork a child process. */
+    if ((pid = fork()) < 0) {     
+		fprintf(stderr, "*** ERROR: forking child process failed\n");
+		exit(1);
+    } else if (pid == 0) {
+    	/* For the child process. */
+
+    	/* Execute the command. */   
+		if (execvp(*argv, argv) < 0) {
+			fprintf(stderr, "%s: Unknown command: %s\n", NAME, argv[0]);
+			exit(1);
+		}
+	} else {
+		/* For the parent. */
+
+    	/* Wait for completion. */
+    	while (wait(&status) != pid);
+	}
+}
+
+void print_prompt(char *wd, size_t size) {
+	/* Fix prompt. */
+	if (getcwd(wd, size) == NULL) {
+		fprintf(stderr, "getcwd: %s\n", strerror(errno));
+	}
+
+	/* Print prompt. */
+	printf(prompt, YELLOW, RESET, BLACK,
+	RESET, MAGENTA, wd, RESET,
+	BLUE, RESET);
 }
